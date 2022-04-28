82a7eb9534d114eb93ddde185aa010b8f5547575
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-29 00:14:51 +0530

    Reads in integers prints them in sorted order using Bubble Sort

---
+++ src/com/bridgelabz/algoritham/BubbleSort.java
@@ -0,0 +1,55 @@
+package com.bridgelabz.algoritham;
+
+import java.util.Scanner;
+
+/**
+ * Reads in integers prints them in sorted order using Bubble Sort
+ * 
+ * @author : Snehal Patil
+ *
+ */
+public class BubbleSort {
+
+	// sorting method to sort the given array
+	public static void bubbleSort(int[] arr) {
+		int temp;
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("-------- Welcome to Bubble Sort Program -----------");
+		System.out.println();
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter how many number you want to sort : ");
+		int size = sc.nextInt();
+		int[] array = new int[size];
+
+		System.out.println("Enter numbers to sort : ");
+		for (int i = 0; i < array.length; i++) {
+			array[i] = sc.nextInt();
+		}
+
+		// Print original array
+		System.out.print("\nArray before sorting : ");
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + ", ");
+		}
+		System.out.println("\n");
+
+		// calling bubble sort method
+		bubbleSort(array);
+		System.out.print("Array after sorting : ");
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + ", ");
+		}
+	}
+}
