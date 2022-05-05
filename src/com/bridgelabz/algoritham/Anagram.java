dfd9cde5d3e9f0f94dc2b5d2c644b7e8048c5cb9
snehalpatil94 <erpatilsnehal@gmail.com>
2022-05-05 21:58:02 +0530

    program to check string is an anagram of another string

---
+++ src/com/bridgelabz/algoritham/Anagram.java
@@ -0,0 +1,47 @@
+package com.bridgelabz.algoritham;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+	/**
+	 * This method checks if two string are anagram.
+	 * 
+	 * @param str1 - input first string
+	 * @param str2 - input second string
+	 * @return
+	 */
+	public static void checkAnagram(char[] str1, char[] str2) {
+		int str_1length = str1.length;
+		int str_2Length = str2.length;
+		boolean check = true;
+		
+		if (str_1length != str_2Length) {
+			System.out.println("The two strings are not anagram of each other");
+		} else {
+			Arrays.sort(str1);
+			Arrays.sort(str2);
+			for (int i = 0; i < str_1length; i++)
+				if (str1[i] != str2[i]) {
+					check = false;
+				}
+			if (check)
+				System.out.println("The two strings are anagram of each other !");
+			else
+				System.out.println("The two strings are not  anagram of each other !");
+		}
+	}
+
+	public static void main(String args[]) {
+		System.out.println("------------ Welcome To Anagram Program -------------");
+		System.out.println();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First String : ");
+		String firstStr = sc.next();
+		System.out.println("Enter First String : ");
+		String secondStr = sc.next();
+
+		checkAnagram(firstStr.toCharArray(), secondStr.toCharArray());
+	}
+}
